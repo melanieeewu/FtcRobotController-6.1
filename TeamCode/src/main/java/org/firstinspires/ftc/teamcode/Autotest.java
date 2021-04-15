d1d2c716aa919e8edd8ce7b6871081e20b9552d7
melanieeewu <73800239+melanieeewu@users.noreply.github.com>
2021-04-15 01:07:54 -0700

    Update Autotest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autotest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autotest.java
@@ -52,85 +52,217 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Autotest", group="Pushbot")
+@Autonomous(name="Pushbot: Auto Drive By Encoder", group="pushbotHardware")
+public class PushbotAutoDriveByEncoder_Linear extends LinearOpMode {
 
-public class Autotest extends LinearOpMode {
+    /* Declare OpMode members. */
+    pushbotHardware robot   = new pushbotHardware();   // Use a Pushbot's hardware
+    private ElapsedTime     runtime = new ElapsedTime();
 
-    // Declare OpMode members.
-    pushbotHardware robot = new pushbotHardware();
-
-    public DcMotor leftFront;   //drivetrainwheels
-    public DcMotor leftBack;
-    public DcMotor rightFront;
-    public DcMotor rightBack;
+    static final double     COUNTS_PER_MOTOR_REV    = 58.012 ;    // eg: TETRIX Motor Encoder
+    static final double    DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   =2.95 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+                                                      (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.5;
+    static final double     TURN_SPEED              = 0.25;
 
     @Override
     public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
 
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
         robot.init(hardwareMap);
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        leftFront = hardwareMap.dcMotor.get("leftFront");
-        leftBack = hardwareMap.dcMotor.get("leftBack");
-        rightBack = hardwareMap.dcMotor.get("rightBack");
-        rightFront = hardwareMap.dcMotor.get("rightFront");
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.update();
 
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        robot.leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        robot.leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
-
         waitForStart();
 
-        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // Step through each leg of the path,
+        // Note: Reverse movement is obtained by setting a negative distance (not speed)
+       // encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        //encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+       // encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+        //encoderDrive(TURN_SPEED,  -12, 12, 4.0);  // S2: Turn  left
 
-       runtime.reset();
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
+      //  robot.leftClaw.setPosition(1.0);            // S4: Stop and close the claw.
+      //  robot.rightClaw.setPosition(0.0);
+       // sleep(1000);     // pause for servos to move
 
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower;
-            double rightPower;
+        robot.lift1.setPosition(0);                                            //starting postion
+        robot.lift2.setPosition(0);
+        robot.lift3.setPosition(0);
+        robot.claw.setPosition(0);
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
+        encoderDrive(DRIVE_SPEED,  36,  36, 5.0);  // go up 3 tiles
+        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // turn right
 
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+        robot.lift1.setPosition(0.8);                                            //drop off wobble goal
+        robot.lift2.setPosition(0.6);
+        robot.lift3.setPosition(0.7);
+        robot.claw.setPosition(1);
+        sleep(1000);
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
 
-            // Send calculated power to wheels
-            leftFront = hardwareMap.dcMotor.get("leftFront");
-            leftBack = hardwareMap.dcMotor.get("leftBack");
-            rightBack = hardwareMap.dcMotor.get("rightBack");
-            rightFront = hardwareMap.dcMotor.get("rightFront");
-            rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
-            rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        robot.lift1.setPosition(0);                                            //drop off wobble goal
+        robot.lift2.setPosition(0);
+        robot.lift3.setPosition(0);
+        robot.claw.setPosition(0);
+        sleep(1000);
 
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-            telemetry.update();
+
+        encoderDrive(DRIVE_SPEED, -12, -12, 4.0);  //goes backwards
+        encoderDrive(TURN_SPEED,   -12, 12, 4.0);  // turns left to shoot
+
+        pulley(0.25);
+        sleep(370);
+
+        pulley(0);
+        sleep(100);
+
+        shootRing(1);    //shoot ring????
+        sleep(1000);
+
+        robot.flicker.setPosition(0);
+        sleep(10);
+
+        shootRing(0.7);    //shoot ring????cv
+        sleep(1000);
+
+        robot.flicker.setPosition(1);
+        sleep(10);
+
+        shootRing(1);    //shoot ring????
+        sleep(1000);
+
+        robot.flicker.setPosition(0);
+        sleep(10);
+
+        shootRing(0.7);    //shoot ring????cv
+        sleep(1000);
+
+        robot.flicker.setPosition(1);
+        sleep(10);
+
+        shootRing(1);    //shoot ring????
+        sleep(1000);
+
+        robot.flicker.setPosition(0);
+        sleep(10);
+
+        shootRing(1);    //shoot ring????cv
+        sleep(1000);
+
+        robot.flicker.setPosition(1);
+        sleep(10);
+
+        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  //park
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+    }
+
+    /*
+     *  Method to perform a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the opmode running.
+     */
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = robot.leftFront.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newLeftTarget = robot.leftBack.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget = robot.rightFront.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            newRightTarget = robot.rightBack.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            robot.leftFront.setTargetPosition(newLeftTarget);
+            robot.leftBack.setTargetPosition(newLeftTarget);
+            robot.rightFront.setTargetPosition(newRightTarget);
+            robot.rightBack.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            robot.leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.leftFront.setPower(Math.abs(speed));
+            robot.leftBack.setPower(Math.abs(speed));
+            robot.rightFront.setPower(Math.abs(speed));
+            robot.rightBack.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                   (runtime.seconds() < timeoutS) &&
+                   (robot.leftFront.isBusy() && robot.rightBack.isBusy())) {
+
+                    robot.claw.setPosition(1);
+
+            }
+
+            // Stop all motion;
+            robot.leftFront.setPower(0);
+            robot.leftBack.setPower(0);
+            robot.rightFront.setPower(0);
+            robot.rightBack.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            robot.leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //  sleep(250);   // optional pause after each move
+
 
         }
+
+
     }
+
+    public void shootRing(double speed){
+        robot.shooter.setPower(speed);
+        robot.shooter2.setPower(speed);
+    }
+
+    public void pulley(double speed) {
+        robot.pulley.setPower(-speed);
+    }
+
 }
+
