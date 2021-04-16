cff86929e4d2426e7a7c328673dbcb5b143ca075
melanieeewu <73800239+melanieeewu@users.noreply.github.com>
2021-04-16 12:26:58 -0700

    Update Autotest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autotest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autotest.java
@@ -51,7 +51,6 @@ import com.qualcomm.robotcore.util.Range;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-
 @Autonomous(name="Pushbot: Auto Drive By Encoder", group="pushbotHardware")
 public class PushbotAutoDriveByEncoder_Linear extends LinearOpMode {
 
